54cf38ffc0ed199f97af9ee09e17f5ee1c026d62
AzzunAdna5 <azzuny5@gmail.com>
2024-06-16 14:01:34 +0700

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,11 @@
+# Deklarasi string
+jml_mangga = "34"
+jml_semangka = "15"
+
+# Penjumlahan jika tipe data string
+total_buah_string = jml_mangga + jml_semangka
+print(total_buah_string)
+
+# Penjumlahan jika tipe data diubah ke int
+total_buah_int = int(jml_mangga) + int(jml_semangka)
+print(total_buah_int)
