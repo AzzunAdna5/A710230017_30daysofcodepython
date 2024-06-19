7bb4440b197b00cc01bd952d44acec996b34381a
AzzunAdna5 <azzuny5@gmail.com>
2024-06-19 22:38:41 +0700

    Create day5.py

---
+++ day5.py
@@ -0,0 +1,14 @@
+angka_1 = input('Masukkan angka 1: ')
+angka_2 = input('Masukkan angka 2: ')
+
+# Konversi input angka menjadi float
+angka_1 = float(angka_1)
+angka_2 = float(angka_2)
+
+# Perhitungan hasil bagi dan modulus
+hasil_bagi = angka_1 / angka_2
+hasil_modulus = angka_1 % angka_2
+
+# Menampilkan hasil
+print('Hasil angka 1 dibagi angka 2 =', hasil_bagi)
+print('Hasil modulus angka 1 dengan angka 2 =', hasil_modulus)
