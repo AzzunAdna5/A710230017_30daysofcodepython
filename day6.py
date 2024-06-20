5e185e1a41c6100f5cfcbc6aedab383b3b5f05d8
AzzunAdna5 <azzuny5@gmail.com>
2024-06-20 07:41:44 +0700

    Create day6.py

---
+++ day6.py
@@ -0,0 +1,6 @@
+kata = 'Pendidikan Teknik Informatika UMS'
+
+print("Hasilnya [:] = ", kata[:])
+print("Hasilnya [8:] = ", kata[8:])
+print("Hasilnya [8:18] = ", kata[8:18])
+print("Hasilnya [:15] = ", kata[:15])
