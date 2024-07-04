e3f580baf16538a39b35b4f6355864001a4c35a5
AzzunAdna5 <azzuny5@gmail.com>
2024-07-04 22:31:17 +0700

    Create day20.py

---
+++ day20.py
@@ -0,0 +1,19 @@
+# Bagian pertama: mencetak angka dari 1 sampai 9
+for i in range(1, 10):
+    print(i, end=' ')
+print()  # Untuk mencetak baris baru setelah selesai mencetak angka
+
+# Bagian kedua: mencetak angka dari 10 sampai 1 secara terbalik
+for j in range(10, 0, -1):
+    print(j, end=' ')
+print()  # Untuk mencetak baris baru setelah selesai mencetak angka
+
+# Bagian ketiga: mencetak angka dari 1 sampai 19 dengan selisih 3
+for k in range(1, 20, 3):
+    print(k, end=' ')
+print()  # Untuk mencetak baris baru setelah selesai mencetak angka
+
+# Bagian keempat: mencetak angka dari 1 sampai 19 dengan selisih 2
+for l in range(1, 20, 2):
+    print(l, end=' ')
+print()  # Untuk mencetak baris baru setelah selesai mencetak angka
